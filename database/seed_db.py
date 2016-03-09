a5af9a5c09ba6503cd4669055692a55f1da045bc
edodge <edodge11@gmail.com>
2016-03-08 21:04:35 -0500

    Finalizing data import script

--- database/seed_db.py
+++ database/seed_db.py
@@ -10,22 +10,55 @@ PASSWORD = CLIENT_SECRETS['password']
 
 ENDPOINT = 'mssql.cxezxmmbhs1j.us-east-1.rds.amazonaws.com'
 
-conn = pymssql.connect(server=ENDPOINT, user=USERNAME, password=PASSWORD, db='turkey_demo', port=1433)
+conn = pymssql.connect(server=ENDPOINT, user=USERNAME, password=PASSWORD, database='turkey_demo', port=1433)
 
 cursor = conn.cursor()
 
-cursor.execute("""
-	CREATE DATABASE turkey_demo;
-""")
+# Drop the table if it already exists
+# cursor.execute("IF OBJECT_ID('nuts_data', 'U') IS NOT NULL DROP TABLE nuts_data;")
+# conn.commit()
 
-cursor.execute("""
-	CREATE TABLE persons (
-	    id INT NOT NULL,
-	    name VARCHAR(100),
-	    salesrep VARCHAR(100),
-	    PRIMARY KEY(id)
-	)
-""")
+# # Create the table
+# cursor.execute("""
+# 	CREATE TABLE nuts_data (
+# 	    region_code VARCHAR(10) NOT NULL,
+# 	    topic VARCHAR(50) NOT NULL,
+# 	    group_id INT NOT NULL,
+# 	    indicator_id INT NOT NULL,
+# 	    n INT,
+# 	    value INT,
+# 	    CONSTRAINT pk_RecordId PRIMARY KEY (region_code,topic,group_id,indicator_id)
+# 	);
+# """)
+# conn.commit()
 
-conn.commit()
+# Populate the data from our .csv files
+file_stubs = [
+	'Corruption',
+	'Crime',
+	'Finance',
+	'Firm Characteristics',
+	'Gender',
+	'Informality',
+	'Infrastructure',
+	'Innovation and Technology',
+	'Performance',
+	'Regulations and Taxes',
+	'Trade'
+	'Workforce'
+]
+
+for stub in file_stubs:
+	print 'Loading data for topic: '+stub
+
+	df = pd.read_csv('../data/nutsData'+stub+'.csv',usecols=['regionCode','groupID','indicatorID','N','value'])
+	df['topic'] = stub
+
+	upload_data = [tuple(x) for x in df[['regionCode','topic','groupID','indicatorID','N','value']].values]
+
+	cursor.executemany('''INSERT INTO nuts_data (region_code, topic, group_id, indicator_id, n, value)
+                      VALUES (%s,%s,%s,%s,%s,%s);''', upload_data)
+	conn.commit()
+
+
 conn.close()
